af44d7332e1a4f589d44471ec84a80e0a709ee47
Ibr707090 <ibrsupreme.604@gmail.com>
2024-10-13 21:01:50 +0530

    Update m.py

--- m.py
+++ m.py
@@ -28,7 +28,7 @@ logging.basicConfig(filename='bot_actions.log', level=logging.INFO,
                     format='%(asctime)s - %(message)s')
 
 # Initialize the bot with the token from environment variables
-TOKEN = "7274578779:AAFPadSmqpo-7p97m6eFLYN21q361QJt1as"
+TOKEN = "7427019526:AAERe5i1jry61AOfWLs-CGOIvF5sHQs9EEk"
 if not TOKEN:
     raise ValueError("Please set your bot token in the environment variables!")
 
